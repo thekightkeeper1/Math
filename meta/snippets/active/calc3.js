b1db987e7eb26401c68fdd093c2f43086c8472ca
Ty <ty.singh828@gmail.com>
2025-06-16 16:12:10 -0400

    vault backup: 2025-06-16 16:12:10

--- meta/snippets/active/calc3.js
+++ meta/snippets/active/calc3.js
@@ -8,8 +8,11 @@
 
     {trigger: "inv", replacement: "^{-1}", options: "mA"},
     {trigger: "fxy=", replacement: "f(x,y) = ", options: "mA"},
-    {trigger: "fxyz=", replacement: "f(x,y,z) = ", options: "mA"}
-    {trigger: "f[xyz]xyz=", replacement: "f(x,y,z) = ", options: "mA"}
+    {trigger: "fxyz=", replacement: "f(x,y,z) = ", options: "mA"},
+    {trigger: "f([xyz])xyz=", replacement: "f_[[0]](x,y,z) = ", options: "rmA"},
+    {trigger: "f([xy])xy=", replacement: "f_[[0]](x,y) = ", options: "rmA"},
+    {trigger: "dd([xyz])", replacement: "\\frac{\\partial}{\\partial [[0]]} ", options: "rmA"},
+
 
 
 ]
--- meta/snippets/active/default.js
+++ meta/snippets/active/default.js
@@ -172,7 +172,7 @@
     {trigger: "...", replacement: "\\dots", options: "mA"},
     {trigger: "nabl", replacement: "\\nabla", options: "mA"},
     {trigger: "del", replacement: "\\nabla", options: "mA"},
-    {trigger: "xx", replacement: "\\times", options: "mA"},
+    // {trigger: "xx", replacement: "\\times", options: "mA"},
     {trigger: "**", replacement: "\\cdot", options: "mA"},
     {trigger: "para", replacement: "\\parallel", options: "mA"},
 
