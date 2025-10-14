dae2a76d58b1f162ff987b4ece86be07f70786ea
Ty <ty.singh828@gmail.com>
2025-10-14 16:24:25 -0400

    vault backup: 2025-10-14 16:24:25

--- meta/snippets/active/calc3.js
+++ meta/snippets/active/calc3.js
@@ -1,10 +1,5 @@
 [
-    // Texting for math
-    {
-        trigger: "  ",
-            replacement: "\\space ",
-        options: "mA",
-    },
+
 
     {trigger: "inv", replacement: "^{-1}", options: "mA"},
     {trigger: "fxy=", replacement: "f(x,y) = ", options: "mA"},
@@ -13,7 +8,6 @@
     {trigger: "f([xy])xy=", replacement: "f_[[0]](x,y) = ", options: "rmA"},
     {trigger: "dd([xyz])", replacement: "\\frac{\\partial}{\\partial [[0]]} ", options: "rmA"},
     {trigger: "lr.|", replacement: "\\left. \\right|", options: "mA"},
-
     {trigger: "osu", replacement: "\\left. \\right|", options: "mA"},
 
 
