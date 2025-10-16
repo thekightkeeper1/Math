ae7e7735479fa510b1913a6e42866b6d848cff7e
thekightkeeper1 <ty.singh828@gmail.com>
2025-10-16 09:30:19 -0400

    vault backup: 2025-10-16 09:30:19

--- meta/snippets/active/diffeq.js
+++ meta/snippets/active/diffeq.js
@@ -4,12 +4,32 @@
     {trigger: "dm", replacement: "$$\n$0\n$$", options: "tAw"},
     {trigger: "([a-zA-Z])mk", replacement: "$[[0]]$", options: "rtAw"},
 
+    // operators
+    {
+        trigger: "^",
+        replacement: "^{$0}$1",
+        options: "mwA"
+    },
 
     // Diffeq
     {
         trigger: "2deq",
-        replacement: `a$0{}y'' + by' + cy = f(t)`,
-        options: "m"
+        replacement: "${1:a}y'' + ${2:b}y' + ${3:c}y = ${4:f(t)}",
+        options: "mA"
     },
+
+
+    // Trig
+    // Trigonometry
+    {trigger: /([^\\])(arcsin|sin|arccos|cos|arctan|tan|csc|sec|cot)/, replacement: "[[0]]\\[[1]]", options: "rmA", description: "Add backslash before trig funcs"},
+
+    {trigger: /\\(arcsin|sin|arccos|cos|arctan|tan|csc|sec|cot)([A-Za-gi-z])/,
+        replacement: "\\[[0]] [[1]]", options: "rmA",
+        description: "Add space after trig funcs. Skips letter h to allow sinh, cosh, etc."},
+
+    {trigger: /\\(sinh|cosh|tanh|coth)([A-Za-z])/,
+        replacement: "\\[[0]] [[1]]", options: "rmA",
+        description: "Add space after hyperbolic trig funcs"},
+
 
 ]
