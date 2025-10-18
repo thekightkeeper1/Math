5dfc3df75efe187f1c08b1c68ef3ddb804030341
thekightkeeper1 <ty.singh828@gmail.com>
2025-10-18 14:41:59 -0400

    vault backup: 2025-10-18 14:41:59

--- meta/snippets/active/diffeq.js
+++ meta/snippets/active/diffeq.js
@@ -5,7 +5,7 @@
     {trigger: "([a-zA-Z])mk", replacement: "$[[0]]$", options: "rtAw"},
     {trigger: /([^'])\b([B-HJ-Zb-z])\b([\n\s.,?!:'])/, replacement: "[[0]]$[[1]]$[[2]]", options: "tA"},
     {trigger: /([A-Za-z])(\d)/, replacement: "$[[0]]_{[[1]]}$", options: "rtA", description: "Auto letter subscript", priority: -1},
-    {trigger: / (?!we|in|of|is|as|at|an|if|it|on|no|by|to|so)([a-z0-9])([a-z0-9])/i, replacement: " $[[0]]([[1]])$ ", options: "t", priority: -1},
+    {trigger: / (?!we|in|of|is|as|at|an|if|it|on|no|by|to|so|or)([a-z0-9])([a-z0-9])/i, replacement: " $[[0]]([[1]])$ ", options: "t", priority: -1},
     {trigger: /([a-z])_([a-z0-9])/ , replacement: "$[[0]]_[[1]]$ ", options: "rt",},
 
     {trigger: /([A-Za-z])(\d)/, replacement: "[[0]]_{[[1]]}", options: "rmA", description: "Auto letter subscript", priority: -1},
@@ -29,11 +29,6 @@
     {
         trigger: "2ode",
         replacement: "ay'' + by' + cy = f(t) ",
-        options: "mA"
-    },
-    {
-     trigger: "yp",
-        replacement: "y_p ",
         options: "mA"
     },
 
